70ac66a55f830db7a9966517744430adc8ab6d75
psdhtmlcss <psdhtmlcss@mail.ru>
2023-01-23 18:31:16 +0400

    editorjs

---
+++ src/index.js
@@ -0,0 +1,57 @@
+'use strict';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import Embed from '@editorjs/embed';
+import SimpleImage from './simple-image/simple-image.js';
+
+const saveButton = document.querySelector('button');
+
+const editor = new EditorJS({
+  holder: 'editorjs',
+  autofocus: true,
+  tools: {
+    header: {
+      class: Header,
+      config: {
+        placeholder: 'Enter a header',
+        levels: [1, 2, 3],
+        defaultLevel: 1
+      }
+    },
+    list: {
+      class: List,
+      inlineToolbar: true,
+      config: {
+        defaultStyle: 'unordered'
+      }
+    },
+    embed: Embed,
+    image: SimpleImage
+  },
+  data: {
+    time: 1552744582955,
+    blocks: [
+      {
+        type: "image",
+        data: {
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/abstract-background_VCJK4NBK4W.jpg"
+        }
+      }
+    ],
+    version: "2.11.10"
+  }
+});
+
+
+
+const onSaveButtonClick = (evt) => {
+  evt.preventDefault();
+  editor.save().then((outputData) => {
+    console.log('Article data: ', outputData)
+  }).catch((error) => {
+    console.log('Saving failed: ', error)
+  });
+}
+
+saveButton.addEventListener('click', onSaveButtonClick);
